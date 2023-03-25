42218a149df772615d7f301cb531cb29e4a4268b
isnendyan <isnendyankp@gmail.com>
2023-03-25 09:24:57 +0700

    Create Element Function & calling function under the hood

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Greeting() {
-  return <h2>My First Component</h2>;
+  return (
+    <div>
+      <h2>My First Component</h2>
+    </div>
+  );
 }
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 

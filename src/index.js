e7f5e9eccc4e2b2dcad34fdf4e8b0b5dc6ea0588
isnendyan <isnendyankp@gmail.com>
2023-03-30 10:32:41 +0700

    learn JSX Javascript

--- src/index.js
+++ src/index.js
@@ -14,32 +14,20 @@ const BookList = () =>{
   );
 }
 
-const Book = () =>{
+const author = 'Jordan Moore';
+const Book = () => {
+  const title = 'Interesting Facts For Curious Mindssssss';
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img 
+    src='https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg'
+    alt='Interesting Facts For Curious Minds' 
+      />
+      <h2>{title}</h2>
+      <h4>{author} </h4>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src='https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg'
-    alt='Interesting Facts For Curious Minds'
-  />
-);
-const Title = () => <h2>Interesting Facts For Curious Minds</h2>;
-const Author = () => {
-  const inlineHeadingStyles = {
-    color: '#617d98',
-    fontSize: '0.75rem',
-    marginTop: '0.5rem',
-  };
-  return <h4 style={inlineHeadingStyles}>Jordan Moore </h4>;
-};
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 

4777ef037aa5ed2a104b4e9d68dc822e2d87908e
isnendyan <isnendyankp@gmail.com>
2023-04-02 08:47:41 +0700

    learn simple proper list

--- src/index.js
+++ src/index.js
@@ -4,49 +4,50 @@ import './index.css';
 // const author = 'Jordan Moore';
 // const title = 'Interesting Facts For Curious Mindssssss';
 
-const firstBook = {
-  author: 'Jordan Moore',
+const books = [
+  {
+    author: 'Jordan Moore',
   title: 'Interesting Facts For Curious Minds',
   img: 'https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg',
-};
-const secondBook = {
-  author: 'James Clear',
-  title: 'Atomic Habits',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
-};
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+  },
+];
 
-function BookList() {
+// const firstBook = {
+//   author: 'Jordan Moore',
+//   title: 'Interesting Facts For Curious Minds',
+//   img: 'https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg',
+// };
+// const secondBook = {
+//   author: 'James Clear',
+//   title: 'Atomic Habits',
+//   img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+// };
+
+const BookList = () => {
   return (
     <section className='booklist'>
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      >
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
-          repudiandae inventore eos qui animi sed iusto alias eius ea sapiente.
-        </p>
-        <button>click me</button>
-      </Book>
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
+      {books.map((book) => {
+        console.log(book);
+        const { img, title, author } = book;
+        return <Book img={img} title={title} author={author} />;
+      })}
     </section>
   );
-}
+};
 
 const Book = (props) => {
-  const { img, title, author, children } = props;
+  const { img, title, author } = props;
   console.log(props);
   return (
     <article className='book'>
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author} </h4>
-      {children}
     </article>
   );
 };

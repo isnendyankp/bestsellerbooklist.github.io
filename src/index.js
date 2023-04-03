05f790951a08c7c0b48e3eaed49c4251e298fc53
isnendyan <isnendyankp@gmail.com>
2023-04-03 07:44:42 +0700

    create handleForm input & handleButtonClick with alert

--- src/index.js
+++ src/index.js
@@ -31,13 +31,24 @@ const BookList = () => {
 };
 
 const EventExamples = () => {
+  const handleFormInput = () => {
+    console.log('handle form input');
+  };
+  const handleButtonClick = () => {
+    alert('handle button click');
+  };
   return (
     <section>
       <form>
         <h2>Typical Form</h2>
-        <input type='text' name='example' style={{ margin: '1rem 0'}} />
+        <input 
+          type='text'
+          name='example'
+          onChange={handleFormInput}
+          style={{ margin: '1rem 0'}} 
+        />
       </form>
-      <button>click me</button>
+      <button onClick={handleButtonClick}>click me</button>
     </section>
   );
 }

12b17cbae753356297d684d8aad9fdad47749c6b
isnendyan <isnendyankp@gmail.com>
2023-04-06 12:49:48 +0700

    complex find book after on click

--- src/index.js
+++ src/index.js
@@ -20,15 +20,15 @@ const books = [
 ];
 
 const BookList = () => {
-  const someValue = 'shakeAndBake';
-  const displayValue = () => {
-    console.log(someValue);
-  }
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id);
+    console.log(book);
+  };
+
   return (
     <section className='booklist'>
       {books.map((book) => {
-        return <Book {...book} key={book.id} displayValue=
-        {displayValue} />;
+        return <Book {...book} key={book.id} getBook={getBook} />;
       })}
     </section>
   );
@@ -37,12 +37,12 @@ const BookList = () => {
 
 
 const Book = (props) => {
-  const { img, title, author, displayValue } = props;
+  const { img, title, author, getBook } = props;
   return (
     <article className='book'>
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <button onClick={displayValue}>click me</button>
+      <button onClick={getBook}>click me</button>
       <h4>{author} </h4>
     </article>
   );

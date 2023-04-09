d5ca06d26a8a5e9c5c099cbe4b6556d1aa45a785
isnendyan <isnendyankp@gmail.com>
2023-04-09 11:11:12 +0700

    separate books source  on other files

--- src/index.js
+++ src/index.js
@@ -1,34 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {books} from './books'
 // const author = 'Jordan Moore';
 // const title = 'Interesting Facts For Curious Mindssssss';
 
-const books = [
-  {
-    author: 'Jordan Moore',
-  title: 'Interesting Facts For Curious Minds',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg',
-  id: 1,
-  },
-  {
-    author: 'James Clear',
-    title: 'Atomic Habits',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
-  id:2,
-  },
-];
+
 
 const BookList = () => {
-  const getBook = (id) => {
-    const book = books.find((book) => book.id === id);
-    console.log(book);
-  };
-
+  
   return (
     <section className='booklist'>
       {books.map((book) => {
-        return <Book {...book} key={book.id} getBook={getBook} />;
+        return <Book {...book} key={book.id} />;
       })}
     </section>
   );
@@ -37,15 +21,13 @@ const BookList = () => {
 
 
 const Book = (props) => {
-  const { img, title, author, getBook, id } = props;
-  const getSingleBook = () => {
-    getBook(id);
-  };
+  const { img, title, author, } = props;
+  
   return (
     <article className='book'>
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <button onClick={() => getBook(id)}>click me</button>
+      
       <h4>{author} </h4>
     </article>
   );
